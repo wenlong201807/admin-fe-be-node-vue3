42d4e25d719f00dc0884b41edd85614811340c4c
dragon <1573511441@qq.com>
2023-03-11 11:30:57 +0800

    feat: 基本架构功能

---
+++ manager-fe/src/config/index.js
@@ -0,0 +1,24 @@
+/**
+ * 环境配置封装
+ */
+const env = import.meta.env.MODE || 'prod';
+const EnvConfig = {
+    dev:{
+        baseApi:'/api',
+        mockApi:'https://www.fastmock.site/mock/ffe674869b1659ff73f75a7c06169e2b/api/'
+    },
+    test:{
+        baseApi:'//test.futurefe.com/api',
+        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+    },
+    prod:{
+        baseApi:'//futurefe.com/api', 
+        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+    }
+}
+export default {
+    env,
+    mock:false,
+    namespace:'manager',
+    ...EnvConfig[env]
+}
